819753554b3ca8b6b7012d3a442dd8d4362d6158
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2024-01-24 14:25:53 -0700

    second commit

---
+++ src/app/inicio/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-lg">Relevancia, Pertinencia y Factibilidad de la Investigación:</h1>
+            <p className="text-lg">La movilidad eléctrica es una de las tendencias más importantes en el sector del transporte, ya que se considera una de las soluciones más viables para reducir las emisiones de gases de efecto invernadero (GEI) y mejorar la calidad del aire en las ciudades.</p>
+
+
+        </div>
+    )
+}
