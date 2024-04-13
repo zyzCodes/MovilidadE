9459e8d6b3d1b60b7e036484a7953e96d8a6a5f7
Diego Arana <106112103+zyzCodes@users.noreply.github.com>
2024-04-13 15:27:00 -0600

    fixed typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Movilidad Electrica Para Transporte de Personal en Queretaro</h1>
+          <h1 className="mb-5 text-5xl font-bold">MOVILIDAD ELÉCTRICA PARA TRANSPORTE DE PERSONAL EN QUERÉTARO</h1>
           <p className="mb-5">ANÁLISIS DE VIABILIDAD ECONÓMICA E IMPACTO AMBIENTAL </p>
           <Link href="inicio/">
 
